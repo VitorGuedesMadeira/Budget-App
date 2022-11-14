e20fc5c7ad692fa5a025bd401eb7307d0f23843e
VitorGuedesMadeira <vgm_rox@hotmail.com>
2022-11-14 13:10:06 -0300

    Add associations

--- app/models/establishment.rb
+++ app/models/establishment.rb
@@ -1,2 +1,4 @@
 class Establishment < ApplicationRecord
+  belongs_to :user
+  has_and_belongs_to_many :groups
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,4 @@
 class Group < ApplicationRecord
+  belongs_to :user
+  has_and_belongs_to_many :establishments
 end
