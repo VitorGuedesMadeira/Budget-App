5da55122c0023094e9e7549a14e8e5883e2d5bcb
VitorGuedesMadeira <vgm_rox@hotmail.com>
2022-11-14 12:44:08 -0300

    Add splash screen controller and define routes

---
+++ app/controllers/splash_screen_controller.rb
@@ -0,0 +1,4 @@
+class SplashScreenController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  resources :establishments
-  resources :groups
   resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
+  resources :groups do
+    resources :establishments  
+  end
   # root "articles#index"
+  root 'splash_screen#index'
 end
