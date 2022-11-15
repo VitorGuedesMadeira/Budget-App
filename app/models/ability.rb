35d945bd974aac9d32707c3d4b62ce70f425e6e0
VitorGuedesMadeira <vgm_rox@hotmail.com>
2022-11-14 23:24:20 -0300

    Add authorization for establishments

---
+++ app/models/ability.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(current_user)
+    return unless current_user.present? # additional permissions for logged in users (they can read their own posts)
+
+    can :manage, User, user: current_user
+    can :manage, Group, user: current_user
+    can :manage, Establishment, user: current_user
+    # return unless current_user.role == 'admin' # additional permissions for administrators
+  end
+end
--- db/migrate/20221114220723_create_groups_establishments_join_table.rb
+++ db/migrate/20221114220723_create_groups_establishments_join_table.rb
@@ -2,11 +2,4 @@ class CreateGroupsEstablishmentsJoinTable < ActiveRecord::Migration[7.0]
   def change
     create_join_table :groups, :establishments
   end
-
-  def change
-    create_table :categories_expenses, id: false do |t|
-      t.belongs_to :category
-      t.belongs_to :expense
-    end
-
 end
