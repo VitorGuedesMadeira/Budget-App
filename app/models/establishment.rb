08d37a10eb2a3afee929717ab7fa775bb903da8f
VitorGuedesMadeira <vgm_rox@hotmail.com>
2022-11-16 17:16:58 -0300

    Add uniqueness

--- app/models/establishment.rb
+++ app/models/establishment.rb
@@ -2,6 +2,6 @@ class Establishment < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :groups
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: { scope: :user }
   validates :amount, numericality: { greater_than_or_equal_to: 0 }
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,6 @@ class Group < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :establishments
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: { scope: :user }
   validates :icon, presence: true
 end
